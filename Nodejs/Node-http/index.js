77c5e31bc97b0cff5cec16b5832d826586ddb1a9
Chetan Dhangar <chetansdhangar@gmail.com>
2020-10-05 09:02:05 +0530

    Node Http Example 2

--- Nodejs/Node-http/index.js
+++ Nodejs/Node-http/index.js
@@ -3,14 +3,52 @@ const fs= require('fs');
 const path=require('path');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = 4000;
 
 const server = http.createServer((req,res) =>{
-    console.log(req.headers);
+    console.log('Request for ' + req.url + 'by method' + req.method);
+    
+    if(req.method == 'GET')
+    {
+        var fileUrl;
+        if(req.url == '/') fileUrl= '/index.html';
+        else  fileUrl=req.url;
 
-    res.statusCode=200;
+        var filePath= path.resolve('./public' + fileUrl);
+        const fileExt= path.extname(filePath);
+
+        if(fileExt == '.html'){
+            fs.exists(filePath , (exists)=>{
+                if(!exists)
+                {
+                    res.statusCode=404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1>Error 404 :' + fileUrl + ' Not Found </h1></body></html>');
+                   
+                    return;
+                }
+                res.statusCode=200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+                
+            });
+        }
+        else{
+            res.statusCode=404;
+            res.setHeader('Content-Type','text/html');
+            res.end('<html><body><h1>Error 404 :' + fileUrl + ' Not a Html File </h1></body></html>')
+
+            return;
+        }
+
+    }
+ else{
+    res.statusCode=404;
     res.setHeader("Content-type", 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
+    res.end('<html><body><h1>Error 404 : ' + req.method + ' Not supported</h1></body></html>');
+
+    return;
+ }
 })
 
 server.listen(port,hostname,()=>{
