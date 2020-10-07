b8692447f8e97ad183d0aec235eb25ac08cf6e6f
Chetan Dhangar <chetansdhangar@gmail.com>
2020-10-07 11:05:15 +0530

    Node MongoDB Example 1

---
+++ Nodejs/Node-mongo/index.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err, client)=>{
+    assert.equal(err,null);
+
+    console.log('Connected correctly to the server');
+
+    const db = client.db(dbname);
+    const collection =db.collection("dishes");
+
+    collection.insertOne({"name":"uthappizza", "desciption":"test"},(err,result)=>{
+        assert.equal(err,null);
+
+        console.log('After insert:\n');
+        console.log(result.ops);
+
+        collection.find({}).toArray((err,docs)=>{
+            assert.equal(err,null);
+
+           console.log("Found:\n");
+           console.log(docs);
+           
+        db.dropCollection('dishes',(err,result)=>{
+            assert.equal(err,null);
+
+            client.close();
+        })
+        })
+    })
+})
