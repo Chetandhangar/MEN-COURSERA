37b72b7d42dd135adea944b4d7ab0c4530c1ecad
Chetan Dhangar <chetansdhangar@gmail.com>
2020-10-08 09:08:30 +0530

    mongoose part 2

--- Nodejs/Node-mongoose/index.js
+++ Nodejs/Node-mongoose/index.js
@@ -7,27 +7,25 @@ const  connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log("Connected correctly to the server ");
 
-    var newDish = Dishes({
+    Dishes.create({
         name: "uthappizza",
         description:"test"
-    });
+    })
+    .then((dish)=>{
+        console.log(dish);
 
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log("Found: \n");
+        console.log(dishes);
 
-            return Dishes.find({});
-        })
-        .then((dishes)=>{
-            console.log("Found: \n");
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 })
