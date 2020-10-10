9d56e58d83e128f1e2ca2bc2e3c4b192389530cb
Chetan Dhangar <chetansdhangar@gmail.com>
2020-10-10 16:43:27 +0530

    Express Session part 1

--- Nodejs/conFusionServer/app.js
+++ Nodejs/conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,12 +32,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store:FileStore()
+}));
 
 function auth(req,res,next){
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user) {
+  if(!req.session.user) {
 
     var authHeader = req.headers.authorization;   //stores req.header.atuhorization info in authheader
     if(!authHeader){                                      //if autentication didint provide in req.header
@@ -51,7 +60,8 @@ function auth(req,res,next){
     var password = auth[1];
  
     if(user == 'admin' && password == 'password'){
-      res.cookie('user','admin',{signed:true});
+      //res.cookie('user','admin',{signed:true});
+      req.session.user = 'admin';
       next(); //authorized
     }
     else{
@@ -62,7 +72,7 @@ function auth(req,res,next){
     }
   }
   else{
-    if(req.signedCookies.user == 'admin'){
+    if(req.session.user == 'admin'){
       next();
     }
     else{
