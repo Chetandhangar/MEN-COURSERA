410c9015f0573c8cd85699f12f6a028fbc1a3eb1
chetan dhangar <chetansdhangar@gmail.com>
2020-11-09 10:35:01 +0530

    Mongoose population

--- Nodejs/conFusionServer/models/dishes.js
+++ Nodejs/conFusionServer/models/dishes.js
@@ -1,67 +1,67 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
+const Currency = mongoose.Types.Currency
 
 const commentSchema = new Schema({
-    rating:{
+    rating: {
         type: Number,
-        min:1,
-        max:5,
+        min: 1,
+        max: 5,
         required: true
-
-     },
-     author:{
-         type: String,
-         required: true
-     },
-    
-     comment:{
-        type: String,
-        required : true
     },
-    
-},
-{
-    timestamps:true
-});
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true,
+    usePushEach: true
+})
+
 
 const dishSchema = new Schema({
-     name:{
-         type: String,
-         required: true,
-         unique: true
-     },
-     description:{
-        type:String,
-        featured:true
-     },
-     image:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true,
+        unique: true
     },
-    category:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    label:{
-        type:String,
-        default:''
+    image: {
+        type: String,
+        required: true
     },
-    price:{
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
         type: Currency,
-        required:true,
-        min:0
+        required: true,
+        min: 0
     },
-    featured:{
-        type:Boolean,
-        default:false
+    featured: {
+        type: Boolean,
+        default: false
     },
-       comments : [commentSchema] 
-    }, {
-      timestamps:true
-});
+    comments: [commentSchema]
+}, {
+    timestamps: true,
+    usePushEach: true
+})
 
-const Dishes = mongoose.model('Dish', dishSchema);
+let Dishes = mongoose.model('Dish', dishSchema);
 
-module.exports=Dishes;
+module.exports = Dishes;
--- Nodejs/conFusionServer/routes/users.js
+++ Nodejs/conFusionServer/routes/users.js
@@ -17,42 +17,53 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
-    if(err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successful!'});
+  User.register(new User({username: req.body.username}),
+      req.body.password, (err, user) => {
+          if(err) {
+              res.statusCode = 500;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({err: err});
+          }
+          else {
+              if (req.body.firstname)
+                  user.firstname = req.body.firstname;
+              if (req.body.lastname)
+                  user.lastname = req.body.lastname;
+              user.save((err, user) => {
+                  if (err) {
+                      res.statusCode = 500;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({err: err});
+                      return ;
+                  }
+                  passport.authenticate('local')(req, res, () => {
+                      res.statusCode = 200;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({success: true, status: 'Registration Successful!'});
+                  });
+              });
+          }
       });
-    }
-  });
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, toekn : token, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-
-router.get('/logout',(req,res,next)=>{
-   if(req.session){
-     req.session.destroy();
-     res.clearCookie();
-     res.redirect('/');
-   }
-   else{
-     var err = new Error("You are not logged in!");
-     err.status=403;
-     next(err);
-   }
+router.get('/logout', (req, res) => {
+  if (req.session) {
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');
+  } else {
+      const err = new Error('You are not logged in!')
+      err.status = 403;
+      next(err);
+  }
 });
 
 module.exports = router;
